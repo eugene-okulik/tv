42d40c8e984e41e19e9c7605a6af97325dbaaae3
tv <garbagedesepticon@gmail.com>
2025-01-22 22:47:54 +0600

    Homework lesson 3

---
+++ homework/vladimir_trembach/hw_3_1.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+
+print(f"Сумма: {sum_result}")
+print(f"Разность: {diff_result}")
+print(f"Произведение: {prod_result}")
---
+++ homework/vladimir_trembach/hw_3_4.py
@@ -0,0 +1,11 @@
+import math
+
+a = 5
+b = 10
+
+arithmetic_mean = (a + b) / 2
+
+geometric_mean = math.sqrt(a * b)
+
+print(f"Среднее арифметическое: {arithmetic_mean}")
+print(f"Среднее геометрическое: {geometric_mean}")
